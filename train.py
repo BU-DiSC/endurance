c13764cf26b1adfc4070d1fef204ea7b43074910
ndhuynh <huynh.n.andy@gmail.com>
2022-10-25 17:35:43 +0000

    Formatting

--- train.py
+++ train.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 import os
 import torch
+import toml
 import logging
 import numpy as np
 from tqdm import tqdm
@@ -26,7 +27,7 @@ class Trainer:
 
         self.model = self._build_model()
         self.model = self.model.to(self.device)
-        self.optimizer, self.scheduler = self._build_optimizer(self.model)
+        self.optimizer = self._build_optimizer(self.model)
         self.train_data, self.test_data = self._build_data()
         self.train_len = self.test_len = 0
         self.loss_fn = torch.nn.MSELoss()
@@ -48,14 +49,14 @@ class Trainer:
         return model
 
     def _build_optimizer(self, model):
-        optimizer = torch.optim.SGD(
+        # optimizer = torch.optim.SGD(
+        #         model.parameters(),
+        #         lr=self.config['train']['learning_rate'])
+        optimizer = torch.optim.Adam(
                 model.parameters(),
                 lr=self.config['train']['learning_rate'])
-        scheduler = torch.optim.lr_scheduler.ExponentialLR(
-                optimizer,
-                gamma=self.config['train']['learning_rate_decay'])
 
-        return optimizer, scheduler
+        return optimizer
 
     def _process_row(self, row):
         labels = np.array(row[0:4], np.float32)
@@ -73,14 +74,10 @@ class Trainer:
 
         return {'label': labels, 'feature': features}
 
-    def _build_data(self):
+    def _build_train(self):
         train_dir = os.path.join(
                 self.config['io']['data_dir'],
                 self.config['train']['dir'])
-        test_dir = os.path.join(
-                self.config['io']['data_dir'],
-                self.config['test']['dir'])
-
         dp_train = (DataPipe
                     .iter
                     .FileLister(train_dir)
@@ -92,6 +89,20 @@ class Trainer:
         if self.config['train']['shuffle'] is True:
             dp_train = dp_train.shuffle()
 
+        train = DataLoader(
+                dp_train,
+                batch_size=self.config['train']['batch_size'],
+                drop_last=self.config['train']['drop_last'],
+                # Unsure if needed but to be safe
+                shuffle=self.config['train']['shuffle'],
+                num_workers=0)
+
+        return train
+
+    def _build_test(self):
+        test_dir = os.path.join(
+                self.config['io']['data_dir'],
+                self.config['test']['dir'])
         dp_test = (DataPipe
                    .iter
                    .FileLister(test_dir)
@@ -102,20 +113,18 @@ class Trainer:
                    .sharding_filter())
         if self.config['test']['shuffle'] is True:
             dp_test = dp_test.shuffle()
-
-        train = DataLoader(
-                dp_train,
-                batch_size=self.config['train']['batch_size'],
-                drop_last=self.config['train']['drop_last'],
-                # Unsure if needed but to be safe
-                shuffle=self.config['train']['shuffle'],
-                num_workers=0)
         test = DataLoader(
                 dp_test,
                 batch_size=self.config['test']['batch_size'],
                 drop_last=self.config['test']['drop_last'],
                 shuffle=self.config['test']['shuffle'],
                 num_workers=0)
+
+        return test
+
+    def _build_data(self):
+        train = self._build_train()
+        test = self._build_test()
 
         return train, test
 
@@ -161,24 +170,27 @@ class Trainer:
 
         return test_loss
 
-    def _checkpoint(self, epoch, loss):
-        save_dir = os.path.join(self.config['io']['data_dir'],
-                                self.config['model']['dir'])
-        os.makedirs(save_dir, exist_ok=True)
+    def _dumpconfig(self, save_dir):
+        with open(os.path.join(save_dir, 'config.toml'), 'w') as fid:
+            toml.dump(self.config, fid)
+
+    def _checkpoint(self, save_dir, epoch, loss):
         save_pt = {'epoch': epoch,
                    'model_state_dict': self.model.state_dict(),
                    'optimizer_state_dict': self.optimizer.state_dict(),
                    'loss': loss}
         torch.save(save_pt, os.path.join(save_dir, 'checkpoint.pt'))
 
-    def _save_model(self):
-        save_dir = os.path.join(self.config['io']['data_dir'],
-                                self.config['model']['dir'])
-        os.makedirs(save_dir, exist_ok=True)
+    def _save_model(self, save_dir):
         torch.save(self.model.state_dict(),
                    os.path.join(save_dir, 'kcost_min.model'))
 
     def train(self):
+        save_dir = os.path.join(self.config['io']['data_dir'],
+                                self.config['model']['dir'])
+        os.makedirs(save_dir, exist_ok=True)
+        self._dumpconfig(save_dir)
+
         loss_min = float('inf')
         early_stop_num = self.config['train']['early_stop_num']
         epsilon = self.config['train']['epsilon']
@@ -194,14 +206,13 @@ class Trainer:
         for epoch in range(max_epochs):
             self.log.info(f'Epoch ({epoch+1}/{max_epochs})')
             self._train_loop()
-            self.scheduler.step()
             curr_loss = self._test_loop()
             self.log.info(f'Test loss: {curr_loss}')
-            self._checkpoint(epoch, curr_loss)
+            self._checkpoint(save_dir, epoch, curr_loss)
             if curr_loss < loss_min:
                 loss_min = curr_loss
                 self.log.info('New minmum loss, saving...')
-                self._save_model()
+                self._save_model(save_dir)
 
             losses.pop(0)
             losses.append(curr_loss)
