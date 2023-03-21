0cc0364dcd10d4b75f8e4c6c4a034347cae516b3
ndhuynh <huynh.n.andy@gmail.com>
2023-03-21 14:15:11 -0400

    Add dropout

--- endure/ltune/model/classic_model.py
+++ endure/ltune/model/classic_model.py
@@ -10,28 +10,35 @@ class ClassicTuner(nn.Module):
                             - config['lsm']['size_ratio']['min']
                             + 1)
 
+        in_dim = config['ltune']['model']['in_dim']
+        hidden_dim = self.params['layer_size']
         modules = []
-        num_feat = config['ltune']['model']['in_dim']
-        out_dim = 1 + size_ratio_range
-        modules.append(nn.Linear(num_feat, out_dim))
+
+        # First layer in
+        modules.append(nn.Linear(in_dim, hidden_dim))
         nn.init.xavier_normal_(modules[-1].weight)
+        modules.append(nn.Dropout(p=config['ltune']['model']['dropout']))
         modules.append(nn.ReLU())
-        for _ in range(self.params['hidden_layers']):
-            modules.append(nn.Linear(out_dim, out_dim))
+
+        # Hidden layers
+        for _ in range(self.params['num_layers']):
+            modules.append(nn.Linear(hidden_dim, hidden_dim))
             nn.init.xavier_normal_(modules[-1].weight)
+            modules.append(nn.Dropout(p=config['ltune']['model']['dropout']))
             modules.append(nn.ReLU())
 
+        # Decision layers
         self.bits = nn.Sequential(
-            nn.Linear(out_dim, 1),
+            nn.Linear(hidden_dim, 1),
             nn.Hardtanh(
                 min_val=config['lsm']['bits_per_elem']['min'],
                 max_val=config['lsm']['bits_per_elem']['max'],
             )
         )
-
         self.size_ratio = nn.Sequential(
-            nn.Linear(out_dim, size_ratio_range),
+            nn.Linear(hidden_dim, size_ratio_range),
         )
+
         self.layers = nn.Sequential(*modules)
 
     def forward(self, x, temp=0.5, hard=False) -> torch.Tensor:
