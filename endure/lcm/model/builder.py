3ff468e75ee802439f907b9317006347991be191
ndhuynh <huynh.n.andy@gmail.com>
2023-10-12 16:05:11 -0400

    [Refactor] QLSM model from config

--- endure/lcm/model/builder.py
+++ endure/lcm/model/builder.py
@@ -1,14 +1,17 @@
 from typing import Any, Optional
-import torch
+
+from torch import nn
 import logging
+import torch
 
 from endure.lcm.model import FlexibleModel, QModel, QIntModel, ClassicModel
+from endure.lcm.model.qlsm_model import QModelRefactor
 
 
 class LearnedCostModelBuilder:
     def __init__(self, config: dict[str, Any]):
-        self._config = config
         self.log = logging.getLogger(self._config["log"]["name"])
+        self._config = config
         self._models = {
             "KLSM": FlexibleModel,
             "QLSM": QModel,
@@ -31,5 +34,35 @@ class LearnedCostModelBuilder:
             self.log.warn("Invalid model architecture. Defaulting to classic")
             return ClassicModel(self._config)
         model = model(self._config)
+
+        return model
+
+    def _build_qmodel(self):
+        num_feats = len(self._config["lcm"]["input_features"])
+        cap_range = (
+            self._config["lsm"]["size_ratio"]["max"] -
+            self._config["lsm"]["size_ratio"]["min"] + 1
+        )
+        embedding_size = self._config["lcm"]["embedding_size"]
+        hidden_length = self._config["lcm"]["hidden_length"]
+        hidden_width = self._config["lcm"]["hidden_width"]
+        dropout = self._config["lcm"]["dropout"]
+        norm_layer = None
+        out_width = len(self._config["lcm"]["output_features"])
+        if self._config["lcm"]["norm_layer"] == "Batch":
+            norm_layer = nn.BatchNorm1d
+        elif self._config["lcm"]["norm_layer"] == "Layer":
+            norm_layer = nn.LayerNorm
+
+        model = QModelRefactor(
+            num_feats=num_feats,
+            capacity_range=cap_range,
+            embedding_size=embedding_size,
+            hidden_length=hidden_length,
+            hidden_width=hidden_width,
+            dropout_percentage=dropout,
+            out_width=out_width,
+            norm_layer=norm_layer,
+        )
 
         return model
--- endure/lcm/model/qlsm_model.py
+++ endure/lcm/model/qlsm_model.py
@@ -1,4 +1,4 @@
-from typing import Any
+from typing import Any, Callable, Optional, Tuple
 
 from torch import nn
 import torch
@@ -86,5 +86,81 @@ class QModel(nn.Module):
 
         inputs = torch.cat([feats, size_ratio, q_cap], dim=-1)
         out = self.cost_layer(inputs)
+
+        return out
+
+
+class QModelRefactor(nn.Module):
+    def __init__(
+        self,
+        num_feats: int,
+        capacity_range: int,
+        embedding_size: int = 8,
+        hidden_length: int = 0,
+        hidden_width: int = 32,
+        dropout_percentage: float = 0,
+        out_width: int = 4,
+        norm_layer: Optional[Callable[..., nn.Module]] = None
+    ) -> None:
+        super().__init__()
+        width = (num_feats - 2) + (2 * embedding_size)
+        if norm_layer is None:
+            norm_layer = nn.BatchNorm1d
+        self.t_embedding = nn.Linear(capacity_range, embedding_size)
+        self.q_embedding = nn.Linear(capacity_range, embedding_size)
+        self.in_norm = norm_layer(width)
+        self.in_layer = nn.Linear(width, hidden_width)
+        self.relu = nn.ReLU(inplace=True)
+        self.dropout = nn.Dropout(p=dropout_percentage)
+        hidden = []
+        hidden.append(nn.Identity())
+        for _ in range(hidden_length):
+            hidden.append(nn.Linear(hidden_width, hidden_width))
+        self.hidden = nn.Sequential(*hidden)
+        self.out_layer = nn.Linear(hidden_width, out_width)
+        self.capacity_range = capacity_range
+        self.num_feats = num_feats
+
+        for module in self.modules():
+            if isinstance(module, nn.Linear):
+                nn.init.xavier_normal_(module.weight)
+
+    def _split_input(self, x: torch.Tensor) -> Tuple[torch.Tensor, torch.Tensor, torch.Tensor]:
+        categorical_bound = self.num_feats - 2
+        feats = x[:, :categorical_bound]
+        capacities = x[:, categorical_bound:]
+
+        if self.training:
+            capacities = capacities.to(torch.long)
+            capacities = F.one_hot(capacities, num_classes=self.capacity_range)
+        else:
+            capacities = torch.unflatten(capacities, 1, (-1, self.capacity_range))
+
+        size_ratio = capacities[:, 0, :]
+        q_cap = capacities[:, 1, :]
+
+        return (feats, size_ratio, q_cap)
+
+    def _forward_impl(self, x: torch.Tensor) -> torch.Tensor:
+        feats, size_ratio, q_cap = self._split_input(x)
+
+        size_ratio = size_ratio.to(torch.float)
+        size_ratio = self.t_embedding(size_ratio)
+
+        q_cap = q_cap.to(torch.float)
+        q_cap = self.q_embedding(q_cap)
+
+        inputs = torch.cat([feats, size_ratio, q_cap], dim=-1)
+
+        out = self.in_layer(inputs)
+        out = self.relu(out)
+        out = self.dropout(out)
+        out = self.hidden(out)
+        out = self.out_layer(out)
+
+        return out
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        out = self._forward_impl(x)
 
         return out
