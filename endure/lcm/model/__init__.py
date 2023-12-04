1d9bfd3f739c3938b171aff4a0d71949efd14a9e
ndhuynh <huynh.n.andy@gmail.com>
2023-12-04 16:22:53 -0500

    [Refactor] Remove circular dependency where ordering of __init__ file mattered

--- endure/lcm/model/__init__.py
+++ endure/lcm/model/__init__.py
@@ -2,5 +2,4 @@ from .classic_model import ClassicModel
 from .flexible_model import FlexModel
 from .qlsm_model import QModel
 from .qlsm_multi_head import QModelMultiHead
-from .builder import LearnedCostModelBuilder
 from .kaplsm_model import KapModel
