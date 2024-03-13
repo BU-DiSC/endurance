84411a04e1b54b851cce3038b912555e13667f7a
ndhuynh <huynh.n.andy@gmail.com>
2024-03-13 13:00:08 -0400

    [Refactor] Add leveling and tiering specific generators with a selection func

--- jobs/bayesian_pipeline.py
+++ jobs/bayesian_pipeline.py
@@ -13,26 +13,13 @@ from botorch.acquisition import ExpectedImprovement, UpperConfidenceBound
 from botorch.acquisition.monte_carlo import qExpectedImprovement
 from botorch.optim import optimize_acqf_mixed
 from botorch.models.transforms import Normalize, Standardize
+from torch.types import Number
 
-from endure.lsm.cost import EndureCost
 from endure.data.io import Reader
-from endure.lsm.types import (
-    LSMDesign,
-    System,
-    Policy,
-    Workload,
-    LSMBounds,
-)
-from endure.lcm.data.generator import (
-    ClassicGenerator,
-    QCostGenerator,
-    YZCostGenerator,
-    KHybridGenerator,
-)
-from endure.lsm.solver.classic_solver import ClassicSolver
-from endure.lsm.solver.qlsm_solver import QLSMSolver
-from endure.lsm.solver.yzlsm_solver import YZLSMSolver
-from endure.lsm.solver.klsm_solver import KLSMSolver
+from endure.lsm.cost import EndureCost
+from endure.lsm.types import LSMDesign, System, Policy, Workload, LSMBounds
+import endure.lcm.data.generator as Gen
+import endure.lsm.solver as Solver
 from jobs.infra.db_log import (
     initialize_database,
     log_new_run,
@@ -48,28 +35,32 @@ def print_best_designs(best_designs: List[Tuple[LSMDesign, float]]) -> None:
         if design.policy == Policy.KHybrid:
             k_values_str = ", ".join(str(k) for k in design.K)
             print(
-                f"Design: h={design.h}, T={design.T}, Policy={design.policy}, K=[{k_values_str}], Cost={cost}"
+                f"Design: h={design.h}, T={design.T}, "
+                f"Policy={design.policy}, K=[{k_values_str}], "
+                f"Cost={cost}"
             )
         else:
             print(
-                f"Design: h={design.h}, T={design.T}, Policy={design.policy}, Q={design.Q}, Y={design.Y},"
+                f"Design: h={design.h}, T={design.T}, "
+                f"Policy={design.policy}, Q={design.Q}, Y={design.Y},"
                 f" Z={design.Z}, Cost={cost}"
             )
     with open("best_designs.txt", "w") as file:
         file.write("All Best Designs Found:\n")
         for design, cost in best_designs:
             file.write(
-                f"Design: h={design.h}, T={design.T}, Policy={design.policy}, Q={design.Q}, "
+                f"Design: h={design.h}, T={design.T}, "
+                f"Policy={design.policy}, Q={design.Q}, "
                 f"Y={design.Y}, Z={design.Z}, Cost={cost}\n"
             )
 
 
 class BayesianPipeline:
     def __init__(self, config: dict) -> None:
-        self.end_time = None
-        self.start_time = None
-        self.conn = None
+        self.end_time: float = 0
+        self.start_time: float = 0
         self.run_id: int = 0
+        self.conn = None
         self.log: logging.Logger = logging.getLogger(config["log"]["name"])
 
         jconfig: dict = config["job"]["BayesianOptimization"]
@@ -91,7 +82,7 @@ class BayesianPipeline:
             jconfig["database"]["db_path"],
         )
         self.db_path = os.path.join(self.output_dir, jconfig["database"]["db_name"])
-        model_type_str = self.jconfig.get("model_type", "Classic")
+        model_type_str = jconfig.get("model_type", "Classic")
         self.model_type = getattr(Policy, model_type_str)
         self.num_k_values = jconfig["num_k_values"]
 
@@ -121,7 +112,7 @@ class BayesianPipeline:
         num_iterations: Optional[int],
         sample_size: Optional[int],
         acqf: Optional[str],
-    ):
+    ) -> None:
         os.makedirs(self.output_dir, exist_ok=True)
         self.conn = initialize_database(self.db_path)
         self.system = system if system is not None else self.system
@@ -135,6 +126,7 @@ class BayesianPipeline:
         self.num_iterations = (
             num_iterations if num_iterations is not None else self.num_iterations
         )
+        assert self.conn is not None
         self.run_id = log_new_run(
             self.conn,
             self.system,
@@ -178,10 +170,13 @@ class BayesianPipeline:
 
         return bounds
 
-    def optimization_loop(self, train_x, train_y, best_y):
+    def optimization_loop(
+        self, train_x: torch.Tensor, train_y: torch.Tensor, best_y: Number
+    ) -> List[Tuple[LSMDesign, Number]]:
         bounds = self.generate_initial_bounds(self.system)
         fixed_feature_list = self._initialize_feature_list(bounds)
         best_designs = []
+
         for i in range(self.num_iterations):
             new_candidates = self.get_next_points(
                 train_x,
@@ -198,9 +193,10 @@ class BayesianPipeline:
             )
             self.log.debug(f"Iteration {i + 1}/{self.num_iterations} complete")
         self.log.debug("Bayesian Optimization completed")
+
         return best_designs
 
-    def _initialize_feature_list(self, bounds):
+    def _initialize_feature_list(self, bounds: torch.Tensor) -> List:
         t_bounds = bounds[:, 1]
         lower_t_bound = int(np.floor(t_bounds[0].item()))
         upper_t_bound = int(np.ceil(t_bounds[1].item()))
@@ -230,7 +226,9 @@ class BayesianPipeline:
 
         return fixed_features_list
 
-    def evaluate_new_candidates(self, new_candidates):
+    def evaluate_new_candidates(
+        self, new_candidates: torch.Tensor
+    ) -> Tuple[List[LSMDesign], List[float]]:
         new_designs = self.create_designs_from_candidates(new_candidates)
 
         costs = [
@@ -244,6 +242,7 @@ class BayesianPipeline:
             )
             for design in new_designs
         ]
+        assert self.conn is not None
         for design, cost in zip(new_designs, costs):
             log_design_cost(self.conn, self.run_id, design, cost)
 
@@ -251,7 +250,7 @@ class BayesianPipeline:
 
     def update_training_data(
         self, train_x, train_y, new_candidates, costs, best_designs
-    ):
+    ) -> Tuple[torch.Tensor, torch.Tensor, Number, List[Tuple[LSMDesign, Number]]]:
         new_target = torch.tensor(costs).unsqueeze(-1)
         train_x = torch.cat([train_x, new_candidates])
         train_y = torch.cat([train_y, new_target])
@@ -259,15 +258,19 @@ class BayesianPipeline:
         best_designs = self._update_best_designs(
             best_designs, new_candidates, new_target
         )
+
         return train_x, train_y, best_y, best_designs
 
-    def create_designs_from_candidates(self, candidates):
+    def create_designs_from_candidates(
+        self, candidates: torch.Tensor
+    ) -> List[LSMDesign]:
+        new_designs = []
         for candidate in candidates:
-            new_designs = self._generate_new_designs_helper(candidate)
+            new_designs += self._generate_new_designs_helper(candidate)
 
         return new_designs
 
-    def _generate_new_designs_helper(self, candidate):
+    def _generate_new_designs_helper(self, candidate: torch.Tensor) -> List[LSMDesign]:
         new_designs = []
         h = candidate[0].item()
         if h == self.system.H:
@@ -324,6 +327,7 @@ class BayesianPipeline:
             self.system, self.workload
         )
         best_design, best_cost = sorted_designs[0][0], sorted_designs[0][1]
+        assert self.conn is not None
         log_run_details(
             self.conn,
             self.run_id,
@@ -334,10 +338,11 @@ class BayesianPipeline:
             best_design,
         )
         self.conn.close()
+
         if sorted_designs:
             return best_design, best_cost, elapsed_time
-        else:
-            return None, None, elapsed_time
+
+        return None, None, elapsed_time
 
     def get_next_points(
         self,
@@ -393,6 +398,7 @@ class BayesianPipeline:
             acqf = qExpectedImprovement(model=single_model, best_f=-best_y)
         else:
             raise ValueError(f"Unknown acquisition function: {acquisition_function}")
+
         candidates, _ = optimize_acqf_mixed(
             acq_function=acqf,
             bounds=bounds,
@@ -403,31 +409,29 @@ class BayesianPipeline:
         )
         return candidates
 
-    def _generate_initial_data(self, n: int = 30) -> Tuple[torch.Tensor, torch.Tensor]:
+    def _generate_initial_data(
+        self, n: int = 30
+    ) -> Tuple[torch.Tensor, torch.Tensor, Number]:
         train_x = []
         train_y = []
-        if self.model_type == Policy.QFixed:
-            generator = QCostGenerator(self.bounds)
-        elif self.model_type == Policy.YZHybrid:
-            generator = YZCostGenerator(self.bounds)
-        elif self.model_type == Policy.KHybrid:
-            generator = KHybridGenerator(self.bounds)
-        else:
-            generator = ClassicGenerator(self.bounds)
+
+        generator_class = Gen.get_generator(self.model_type)
+        generator = generator_class(self.bounds)
+
         for _ in range(n):
             design = generator._sample_design(self.system)
+            x_vals = np.array([design.h, design.T])
             if self.model_type == Policy.Classic:
                 policy = 0 if design.policy == Policy.Tiering else 1
-                x_values = np.array([design.h, design.T, policy])
+                x_vals = np.concatenate((x_vals, [policy]))
             elif self.model_type == Policy.QFixed:
-                x_values = np.array([design.h, design.T, design.Q])
+                x_vals = np.concatenate((x_vals, [design.Q]))
             elif self.model_type == Policy.YZHybrid:
-                x_values = np.array([design.h, design.T, design.Y, design.Z])
+                x_vals = np.array((x_vals, [design.Y, design.Z]))
             elif self.model_type == Policy.KHybrid:
-                k_values_padded = (design.K + [1] * self.num_k_values)[
-                    : self.num_k_values
-                ]
-                x_values = np.array([design.h, design.T] + k_values_padded)
+                k_values_padded = design.K + [1] * self.num_k_values
+                k_values_padded = k_values_padded[: self.num_k_values]
+                x_vals = np.concatenate((x_vals, k_values_padded))
             cost = self.cf.calc_cost(
                 design,
                 self.system,
@@ -436,9 +440,11 @@ class BayesianPipeline:
                 self.workload.q,
                 self.workload.w,
             )
+            assert self.conn is not None
             log_design_cost(self.conn, self.run_id, design, cost)
-            train_x.append(x_values)
+            train_x.append(x_vals)
             train_y.append(cost)
+
         train_x = np.array(train_x)
         train_x = torch.tensor(train_x)
         train_y = torch.tensor(train_y, dtype=torch.float64).unsqueeze(-1)
@@ -453,55 +459,23 @@ class BayesianPipeline:
         new_y: torch.Tensor,
     ) -> List[Tuple[LSMDesign, float]]:
         for x, y in zip(new_x, new_y):
+            kwargs = {
+                "h": x[0].item(),
+                "T": np.ceil(x[1].item()),
+                "policy": self.model_type,
+            }
             if self.model_type == Policy.QFixed:
-                h, size_ratio, qvalue = x[0], x[1], x[2]
-                best_designs.append(
-                    (
-                        LSMDesign(
-                            h=h.item(),
-                            T=np.ceil(size_ratio.item()),
-                            policy=Policy.QFixed,
-                            Q=qvalue.item(),
-                        ),
-                        y.item(),
-                    )
-                )
+                kwargs["Q"] = x[2].item()
             elif self.model_type == Policy.YZHybrid:
-                h, size_ratio, yvalue, zvalue = x[0], x[1], x[2], x[3]
-                best_designs.append(
-                    (
-                        LSMDesign(
-                            h=h.item(),
-                            T=np.ceil(size_ratio.item()),
-                            policy=Policy.YZHybrid,
-                            Y=yvalue.item(),
-                            Z=zvalue.item(),
-                        ),
-                        y.item(),
-                    )
-                )
+                kwargs["Y"] = x[2].item()
+                kwargs["Z"] = x[3].item()
             elif self.model_type == Policy.KHybrid:
-                h, size_ratio = x[0], x[1]
-                k_values = x[2:].tolist()
-                design = LSMDesign(
-                    h=h.item(),
-                    T=np.ceil(size_ratio.item()),
-                    policy=Policy.KHybrid,
-                    K=k_values,
-                )
-                best_designs.append((design, y.item()))
-            else:
-                h, size_ratio, policy = x[0], x[1], x[2]
-                if policy.item() < 0.5:
-                    pol = Policy.Leveling
-                else:
-                    pol = Policy.Tiering
-                best_designs.append(
-                    (
-                        LSMDesign(h=h.item(), T=np.ceil(size_ratio.item()), policy=pol),
-                        y.item(),
-                    )
-                )
+                kwargs["K"] = x[2:].tolist()
+            else:  # self.model_type == Policy.Classic
+                pol = Policy.Leveling if x[2].item() < 0.5 else Policy.Tiering
+                kwargs["policy"] = pol
+            best_designs.append((LSMDesign(**kwargs), y.item()))
+
         return best_designs
 
     def _find_analytical_results(
@@ -509,44 +483,43 @@ class BayesianPipeline:
     ) -> Tuple[LSMDesign, float]:
         bounds = bounds if bounds is not None else self.bounds
         if self.model_type == Policy.Classic:
-            solver = ClassicSolver(bounds)
+            solver = Solver.ClassicSolver(bounds)
         elif self.model_type == Policy.QFixed:
-            solver = QLSMSolver(bounds)
+            solver = Solver.QLSMSolver(bounds)
         elif self.model_type == Policy.YZHybrid:
-            solver = YZLSMSolver(bounds)
+            solver = Solver.YZLSMSolver(bounds)
         elif self.model_type == Policy.KHybrid:
-            solver = KLSMSolver(bounds)
+            solver = Solver.KLSMSolver(bounds)
+        else:
+            raise KeyError(f"Solver for {self.model_type} not implemented")
+
         z0, z1, q, w = workload.z0, workload.z1, workload.q, workload.w
-        nominal_design, nominal_solution = solver.get_nominal_design(
-            system, z0, z1, q, w
-        )
+        opt_design, _ = solver.get_nominal_design(system, z0, z1, q, w)
 
         if self.model_type == Policy.Classic:
-            x = np.array([[nominal_design.h, nominal_design.T]])
-            policy = nominal_design.policy
-            cost = solver.nominal_objective(x[0], policy, system, z0, z1, q, w)
+            x = np.array([opt_design.h, opt_design.T])
+            policy = opt_design.policy
+            assert isinstance(solver, Solver.ClassicSolver)
+            cost = solver.nominal_objective(x, policy, system, z0, z1, q, w)
         elif self.model_type == Policy.QFixed:
-            x = np.array([[nominal_design.h, nominal_design.T, nominal_design.Q]])
-            cost = solver.nominal_objective(x[0], system, z0, z1, q, w)
-        elif self.model_type == Policy.YZHybrid:
-            x = np.array(
-                [
-                    [
-                        nominal_design.h,
-                        nominal_design.T,
-                        nominal_design.Y,
-                        nominal_design.Z,
-                    ]
-                ]
-            )
-            cost = solver.nominal_objective(x[0], system, z0, z1, q, w)
-        elif self.model_type == Policy.KHybrid:
-            k_values = nominal_design.K
-            x = np.array([nominal_design.h, nominal_design.T] + k_values)
+            x = np.array([opt_design.h, opt_design.T, opt_design.Q])
+            assert isinstance(solver, Solver.QLSMSolver)
             cost = solver.nominal_objective(x, system, z0, z1, q, w)
+        elif self.model_type == Policy.YZHybrid:
+            x = np.array([opt_design.h, opt_design.T, opt_design.Y, opt_design.Z])
+            assert isinstance(solver, Solver.YZLSMSolver)
+            cost = solver.nominal_objective(x, system, z0, z1, q, w)
+        elif self.model_type == Policy.KHybrid:
+            x = np.array([opt_design.h, opt_design.T] + opt_design.K)
+            assert isinstance(solver, Solver.KLSMSolver)
+            cost = solver.nominal_objective(x, system, z0, z1, q, w)
+        else:
+            raise KeyError(f"Unknown model type {self.model_type}")
+
         print("Cost for the nominal design using analytical solver: ", cost)
-        print("Nominal Design suggested by analytical solver: ", nominal_design)
-        return nominal_design, cost
+        print("Nominal Design suggested by analytical solver: ", opt_design)
+
+        return opt_design, cost
 
 
 if __name__ == "__main__":
--- jobs/infra/db_log.py
+++ jobs/infra/db_log.py
@@ -1,12 +1,13 @@
 import sqlite3
 from endure.data.io import Reader
-from endure.lsm.types import LSMDesign, Policy
+from endure.lsm.types import LSMDesign, Policy, System, Workload
 
 
-def initialize_database(db_path='cost_log.db'):
+def initialize_database(db_path: str = "cost_log.db") -> sqlite3.Connection:
     connector = sqlite3.connect(db_path)
     cursor = connector.cursor()
-    cursor.execute('''
+    cursor.execute(
+        """
         CREATE TABLE IF NOT EXISTS runs (
             run_id INTEGER PRIMARY KEY AUTOINCREMENT,
             empty_reads REAL,
@@ -22,8 +23,10 @@ def initialize_database(db_path='cost_log.db'):
             iterations INT,
             sample_size INT,
             acquisition_function TEXT
-        );''')
-    cursor.execute('''
+        );"""
+    )
+    cursor.execute(
+        """
         CREATE TABLE IF NOT EXISTS design_costs (
             idx INTEGER PRIMARY KEY AUTOINCREMENT,
             run_id INTEGER,
@@ -39,8 +42,10 @@ def initialize_database(db_path='cost_log.db'):
             k16 REAL, k17 REAL, k18 REAL, k19 REAL, k20 REAL,
             cost REAL,
             FOREIGN KEY (run_id) REFERENCES runs(run_id)
-        );''')
-    cursor.execute('''
+        );"""
+    )
+    cursor.execute(
+        """
         CREATE TABLE IF NOT EXISTS run_details (
             run_id INTEGER PRIMARY KEY,
             duration_secs INTEGER,
@@ -59,8 +64,10 @@ def initialize_database(db_path='cost_log.db'):
             bayesian_Y INTEGER,
             bayesian_Z INTEGER,
             FOREIGN KEY (run_id) REFERENCES runs(run_id)
-        );''')
-    cursor.execute('''
+        );"""
+    )
+    cursor.execute(
+        """
         CREATE TABLE IF NOT EXISTS run_details_k_values (
             idx INTEGER PRIMARY KEY AUTOINCREMENT,
             run_id INTEGER,
@@ -69,56 +76,153 @@ def initialize_database(db_path='cost_log.db'):
             k11 REAL, k12 REAL, k13 REAL, k14 REAL, k15 REAL,
             k16 REAL, k17 REAL, k18 REAL, k19 REAL, k20 REAL,
             FOREIGN KEY (run_id) REFERENCES runs(run_id)
-        );''')
+        );"""
+    )
 
     connector.commit()
+
     return connector
 
 
-def log_new_run(connector, system, workload, iterations, sample, acqf):
+def log_new_run(
+    connector: sqlite3.Connection,
+    system: System,
+    workload: Workload,
+    iterations: int,
+    sample: int,
+    acqf: str,
+) -> int:
     cursor = connector.cursor()
-    cursor.execute('INSERT INTO runs (empty_reads, non_empty_reads, range_queries, writes, '
-                   'max_bits_per_element, physical_entries_per_page, range_selectivity, '
-                   'entries_per_page, total_elements, read_write_asymmetry, iterations, sample_size, '
-                   'acquisition_function) '
-                   'VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)',
-                   (workload.z0,  workload.z1, workload.q, workload.w, system.H, system.E, system.s, system.B,
-                    system.N, system.phi, iterations, sample, acqf))
+    cursor.execute(
+        """
+        INSERT INTO runs (
+            empty_reads,
+            non_empty_reads,
+            range_queries,
+            writes,
+            max_bits_per_element,
+            physical_entries_per_page,
+            range_selectivity,
+            entries_per_page,
+            total_elements,
+            read_write_asymmetry,
+            iterations,
+            sample_size,
+            acquisition_function
+        )
+        VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)
+        """,
+        (
+            workload.z0,
+            workload.z1,
+            workload.q,
+            workload.w,
+            system.H,
+            system.E,
+            system.s,
+            system.B,
+            system.N,
+            system.phi,
+            iterations,
+            sample,
+            acqf,
+        ),
+    )
     connector.commit()
+
+    assert cursor.lastrowid is not None
     return cursor.lastrowid
 
 
-def log_design_cost(connector, run_id, design, cost):
+def log_design_cost(
+    connector: sqlite3.Connection,
+    run_id: int,
+    design: LSMDesign,
+    cost: float,
+) -> None:
     cursor = connector.cursor()
     policy = design.policy
-    k_values = design.K + [None] * (20 - len(design.K))  # TODO replace this with the max_levels
-    sql_command = ('INSERT INTO design_costs (run_id, bits_per_element, size_ratio, policy, Q, Y, Z, cost, ' +
-                   ', '.join([f'k{i+1}' for i in range(20)]) + ') ' + 'VALUES (?, ?, ?, ?, ?, ?, ?, ?'', ' +
-                   ', '.join(['?']*20) + ')')
-    cursor.execute(sql_command, (run_id, design.h, design.T, policy.name, design.Q, design.Y, design.Z, cost) +
-                   tuple(k_values))
+    k_values = design.K + [None] * (
+        20 - len(design.K)
+    )  # TODO replace this with the max_levels
+    sql_command = (
+        "INSERT INTO design_costs (run_id, bits_per_element, size_ratio, policy, Q, Y, Z, cost, "
+        + ", ".join([f"k{i+1}" for i in range(20)])
+        + ") "
+        + "VALUES (?, ?, ?, ?, ?, ?, ?, ?"
+        ", " + ", ".join(["?"] * 20) + ")"
+    )
+    cursor.execute(
+        sql_command,
+        (run_id, design.h, design.T, policy.name, design.Q, design.Y, design.Z, cost)
+        + tuple(k_values),
+    )
     connector.commit()
 
 
-def log_run_details(connector, run_id, duration, analytical_cost, bayesian_cost, analytical_design, bayesian_design):
+def log_run_details(
+    connector: sqlite3.Connection,
+    run_id: int,
+    duration: float,
+    analytical_cost: float,
+    bayesian_cost: float,
+    analytical_design: LSMDesign,
+    bayesian_design: LSMDesign,
+) -> None:
     cursor = connector.cursor()
     analytical_policy = analytical_design.policy
     print(analytical_policy)
     bayesian_policy = bayesian_design.policy
-    cursor.execute('''
-        INSERT INTO run_details (run_id, duration_secs, analytical_cost, bayesian_cost, analytical_h, analytical_T, 
-        analytical_policy, analytical_Q, analytical_Y, analytical_Z, bayesian_h, bayesian_T, bayesian_policy, bayesian_Q, 
-        bayesian_Y, bayesian_Z) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)''',
-                   (run_id, duration, analytical_cost, bayesian_cost, analytical_design.h, analytical_design.T,
-                    analytical_policy.name, analytical_design.Q, analytical_design.Y, analytical_design.Z,
-                    bayesian_design.h, bayesian_design.T, bayesian_policy.name, bayesian_design.Q,
-                    bayesian_design.Y, bayesian_design.Z))
+    cursor.execute(
+        """
+        INSERT INTO run_details (
+            run_id,
+            duration_secs,
+            analytical_cost,
+            bayesian_cost,
+            analytical_h,
+            analytical_T,
+            analytical_policy,
+            analytical_Q,
+            analytical_Y,
+            analytical_Z,
+            bayesian_h,
+            bayesian_T,
+            bayesian_policy,
+            bayesian_Q,
+            bayesian_Y,
+            bayesian_Z
+        ) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)""",
+        (
+            run_id,
+            duration,
+            analytical_cost,
+            bayesian_cost,
+            analytical_design.h,
+            analytical_design.T,
+            analytical_policy.name,
+            analytical_design.Q,
+            analytical_design.Y,
+            analytical_design.Z,
+            bayesian_design.h,
+            bayesian_design.T,
+            bayesian_policy.name,
+            bayesian_design.Q,
+            bayesian_design.Y,
+            bayesian_design.Z,
+        ),
+    )
     if bayesian_policy == Policy.KHybrid:
         k_values = bayesian_design.K + [None] * (20 - len(bayesian_design.K))
-        cursor.execute('''
-            INSERT INTO run_details_k_values (run_id, k1, k2, k3, k4, k5, k6, k7, k8, k9, k10, k11, k12, k13, k14, k15, 
-            k16, k17, k18, k19, k20) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)''',
-                       (run_id,) + tuple(k_values))
+        cursor.execute(
+            """
+            INSERT INTO run_details_k_values (
+                run_id, k1, k2, k3, k4, k5, k6, k7, k8, k9, k10,
+                k11, k12, k13, k14, k15, k16, k17, k18, k19, k20
+            ) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)
+            """,
+            (run_id,) + tuple(k_values),
+        )
     connector.commit()
 
 
